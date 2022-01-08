b69739cf15a778da4ebb341fba3282106a3d80c6
Robel <rofikre@yahoo.com>
2022-01-08 12:26:40 -0500

    added and linked JS file in assets folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,6 @@ fetch("https://api.bestbuy.com/v1/products(name=iphone*)?show=salePrice&apiKey=O
   });
 });
 
-
 //bingmaps api - request to find a departmentstores with area codes 
 fetch("https://dev.virtualearth.net/REST/v1/LocalSearch/?type=DepartmentStores&Locations?postalCode=22003&key=AlhvFsP11Bn7teegeVUFvdrw312hORJoNPDjnSGs5qZk5S9lwrAspUQw5wk9wi9I")
  .then(function(response) {
