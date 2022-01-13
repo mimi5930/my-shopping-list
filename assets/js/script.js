d5f78b4eca5a4b3e07472de8d4ac1399b8bcc472
Mike <mikej.miller440@gmail.com>
2022-01-13 01:13:05 -0600

    Added changes from Feature-map directly

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,29 +8,28 @@ var totalPriceE1 =document.querySelector('#total-price');
 var searchHistoryItemArr = []
 var searchHistoryPriceArr = []
 
-// bestbuy api - request to find a departmentstores with area codes
-fetch("https://api.bestbuy.com/v1/products(name=iphone*)?show=salePrice&apiKey=Ou7MZjAsEdRGa1vhKpsui9Xg")
- .then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
+var locationDataArr = []
 
-//bingmaps api - request to find a departmentstores with area codes 
-fetch("https://dev.virtualearth.net/REST/v1/LocalSearch/?type=DepartmentStores&Locations?postalCode=22003&key=AlhvFsP11Bn7teegeVUFvdrw312hORJoNPDjnSGs5qZk5S9lwrAspUQw5wk9wi9I")
- .then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
+var getLoc = function() {
+  //replace 55423 with user input zip code
+  var bestbuyapiUrl = "https://api.bestbuy.com/v1/stores(area(55423,50))?&format=json&show=storeId,storeType,lat,lng,distance&apiKey=" + bestbuyApiKey;
 
-//best api - request to find a stores with postal code with in 10 mile raduis 
-fetch("https://api.bestbuy.com/v1/stores(area(55423,10))?show=storeId,name,hours,distance&apiKey=Ou7MZjAsEdRGa1vhKpsui9Xg")
- .then(function(response) {
-    response.json().then(function(data) {
-      console.log(data);
-    });
-});
+  fetch(bestbuyapiUrl)
+    .then(function(response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      };
+    }
+  );
+};
+
+var saveToLocationDataArr = function(){
+  localStorage.setItem("Location Lat", JSON.stringify(locationDataArr))
+  localStorage.setItem("Location Lng", JSON.stringify(locationDataArr))
+} 
 
 var getFromHistoryArr = function(){
   var localItem = JSON.parse(localStorage.getItem("Item History"));
